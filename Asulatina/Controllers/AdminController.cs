1e0469811c4253b412ff7c637ae913ce0a3ce07d
damonber <damonber@gmail.com>
2013-07-30 00:08:15 -0600

    Added Promotions, Admin pages

---
+++ Asulatina/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asulatina.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View("LoanRequest");
+        }
+
+        public ActionResult LoanRequest()
+        {
+            return View("LoanRequest");
+        }
+
+        public ActionResult AccountStatus()
+        {
+            return View("AccountStatus");
+        }
+
+
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Index", "Site");
+        }
+    }
+}
---
+++ Asulatina/Controllers/SiteController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asulatina.Controllers
+{
+    public class SiteController : Controller
+    {
+        //
+        // GET: /Site/
+
+        public ActionResult Index()
+        {
+            return View("Home");
+        }
+
+        public ActionResult Promotions()
+        {
+            return View("Promotions");
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View("Home");
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View("Home");
+        }
+
+    }
+}
